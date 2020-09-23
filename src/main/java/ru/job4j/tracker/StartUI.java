3d4710e583869d38b204d62c0bd46c4de7146999
andrejborcenko <andrei-borchenko@mail.ru>
2020-09-23 08:54:48 +0300

    Изменить программу Tracker из 2-го модуля [#308202]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -83,7 +83,7 @@ public class StartUI {
         } else System.out.println("Not found");
     }
 
-    public void init(Input input, Tracker tracker, ArrayList<UserAction> actions) {
+    public void init(Input input, Tracker tracker, List<UserAction> actions) {
         boolean run = true;
         while (run) {
             this.showMenu(actions);
@@ -97,7 +97,7 @@ public class StartUI {
         }
     }
 
-    private void showMenu(ArrayList<UserAction> actions) {
+    private void showMenu(List<UserAction> actions) {
         out.println("Menu.");
         for (int index = 0; index < actions.size(); index++) {
             out.println(index + ". " + actions.get(index).name());
@@ -124,7 +124,7 @@ public class StartUI {
         Output output = new ConsoleOutput();
         Input input = new ValidateInput(output, new ConsoleInput());
         Tracker tracker = new Tracker();
-        ArrayList<UserAction> actions = new ArrayList<>();
+        List<UserAction> actions = new ArrayList<>();
                 actions.add(new CreateAction(output));
                 actions.add(new ExitAction());
                 actions.add(new FindByIdAction(output));
