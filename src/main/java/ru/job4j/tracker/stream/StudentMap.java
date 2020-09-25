3702b6c401e9f0b7e4b470d75617b7e507468e54
andrejborcenko <andrei-borchenko@mail.ru>
2020-09-25 21:17:04 +0300

    Преобразование List в Map. [#308055]

---
+++ src/main/java/ru/job4j/tracker/stream/StudentMap.java
@@ -0,0 +1,13 @@
+package ru.job4j.tracker.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class StudentMap {
+    List<Student> students = new ArrayList<>();
+    Map<String, Student> stdMap = students.stream().collect(Collectors
+            .toMap(student -> student.getSurname(),
+                    student -> student));
+}
