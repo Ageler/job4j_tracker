c505bcd6fc59b464d8a264a9e46ff3df304b5aa2
andrejborcenko <andrei-borchenko@mail.ru>
2020-09-25 19:45:13 +0300

    Отладка лямбда в IDEA [#308058]

---
+++ src/main/java/ru/job4j/tracker/stream/StreamMyUsage.java
@@ -0,0 +1,15 @@
+package ru.job4j.tracker.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamMyUsage {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, -4, -8, 0);
+        List<Integer> filtNum = numbers.stream().filter(number -> number >= 0)
+                .collect(Collectors.toList());
+        filtNum.forEach(System.out::println);
+    }
+
+}
