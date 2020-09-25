da770a708e0c04a571540fa62bdf1ae53d1bfaae
andrejborcenko <andrei-borchenko@mail.ru>
2020-09-25 14:28:15 +0300

    Зона видимости в лямбда-выражениях и исключения [#308042]

--- src/main/java/ru/job4j/tracker/lambda/Diapazon.java
+++ src/main/java/ru/job4j/tracker/lambda/Diapazon.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.function.Function;
 
 public class Diapazon {
-     List<Double> diapason(int start, int end, Function<Double, Double> func) {
+    List<Double> diapason(int start, int end, Function<Double, Double> func) {
          List<Double> rsl = new ArrayList<>();
          for (int i = start; i < end; i++) {
              rsl.add(func.apply((double) i));
--- src/test/java/ru/job4j/tracker/lambda/DiapazonTest.java
+++ src/test/java/ru/job4j/tracker/lambda/DiapazonTest.java
@@ -6,14 +6,18 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
-//public class DiapazonTest extends TestCase {
-//    @Test
-//    public void whenLinearFunctionThenLinearResults() {
-//        List<Double> result = diapason(5, 8, x -> 2 * x + 1);
-//        List<Double> expected = Arrays.asList(11D, 13D, 15D);
-//        assertThat(result, is(expected));
-//    }
+public class DiapazonTest extends TestCase {
+
+    @Test
+    public void testwhenLinearFunctionThenLinearResults() {
+        Function<Double, Double> func = x -> 2 * x + 1;
+       List<Double> result = new ArrayList<>();
+        List<Double> expected = Arrays.asList(11D, 13D, 15D);
+        assertThat(result, is(expected));
+    }
+}
