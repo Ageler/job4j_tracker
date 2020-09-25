0024a3281a613347438b6d01592ce7f635634bf8
andrejborcenko <andrei-borchenko@mail.ru>
2020-09-25 14:36:25 +0300

    Подсчет функции в диапазоне. [#308038]

--- src/test/java/ru/job4j/tracker/lambda/DiapazonTest.java
+++ src/test/java/ru/job4j/tracker/lambda/DiapazonTest.java
@@ -15,9 +15,25 @@ public class DiapazonTest extends TestCase {
 
     @Test
     public void testwhenLinearFunctionThenLinearResults() {
-        Function<Double, Double> func = x -> 2 * x + 1;
-       List<Double> result = new ArrayList<>();
+        Diapazon function = new Diapazon();
+        List<Double> result = function.diapason(5, 8, x -> 2 * x + 1);
         List<Double> expected = Arrays.asList(11D, 13D, 15D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void testwhenQuadraticFunctionThenQuadraticResults() {
+        Diapazon function = new Diapazon();
+        List<Double> result = function.diapason(1, 4, x -> x * x);
+        List<Double> expected = Arrays.asList(1D, 4D, 9D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void testwhenIndicativeFunctionThenIndicativeResults() {
+        Diapazon function = new Diapazon();
+        List<Double> result = function.diapason(1, 5, x -> Math.pow(5, x));
+        List<Double> expected = Arrays.asList(5D, 25D, 125D, 625D);
         assertThat(result, is(expected));
     }
 }
